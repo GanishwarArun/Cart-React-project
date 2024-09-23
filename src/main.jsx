e26b7920bfe61116b5f12c38ad959235bde9ac9a
GanishwarArun <ganishwarips@gmail.com>
2024-09-23 16:07:09 +0530

    Edited Contect/Cart

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDom from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { CartProvider } from "./context/cart.jsx";
+import { CartProvider } from "./Context/Cart.jsx";
 
 ReactDom.createRoot(document.getElementById('root')).render(
   <CartProvider>
